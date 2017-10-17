2ea5bb9b7aef5d6881ff72f2b2b2796a994caf4f
Johann Kerr <johannkerr@gmail.com>
2017-10-17 09:07:25 -0400

    updating backend

--- react/movielistr-backend/db/schema.rb
+++ react/movielistr-backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016142902) do
+ActiveRecord::Schema.define(version: 20171017130132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 20171016142902) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "movies", "users"
 end
